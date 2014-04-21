782a7d236999c9aa9d003cbb0c6c9ec5cb3e4a4a
czcz1024 <czcz1024@gmail.com>
2014-04-21 11:47:13 +0800

    Ajax.RenderAction

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    using System.Threading;
+
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Post(string str)
+        {
+            return Content(str);
+        }
+
+        public ActionResult AjaxLoad()
+        {
+            Thread.Sleep(3000);
+            return View();
+            //return Content("ajax load " + DateTime.Now);
+        }
+
+        public ActionResult AjaxLoad2()
+        {
+            return Content("ajax load " + DateTime.Now);
+        }
+    }
+}
